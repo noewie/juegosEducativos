d9943a10d0b7e0e95332fb56aae85ec9f664a51f
Noe <noelia.rocu@gmail.com>
2023-11-01 12:20:32 +0100

    Add files via upload
    starting a new project based in games for children

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import HomePage from "./pages/homepage";
+
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Juegos Educativos</h1>
+      </header>
+      <main>
+        <HomePage/>
+      </main>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+const HomePage = () => {
+    return (
+      <>
+        <h2>Escoge un juego</h2>
+        <ul>
+          <li>Juego1</li>
+          <li>Juego2</li>
+          <li>Juego3</li>
+        </ul>
+      </>
+    );
+  };
+  
+  export default HomePage;
